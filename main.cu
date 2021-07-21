33e3dcb8f98afd7fd64ce5548d7f3540194d0cb3
mad360 <matt@dldipaolo.com>
2021-07-21 19:02:26 -0400

    continue

--- main.cu
+++ main.cu
@@ -65,7 +65,7 @@ void hashTreeP
 		SHA1(nodes[idx].hash,buffer,MESSAGE_SIZE*arities[1]);
 		if (idx == 0){
 			for (int i=0;i<HASH_SIZE;i++)
-				printf("%02x",nodes[idx].hash[i]);
+				printf("%02x",buffer[i]);	
 		}
 
 	}
